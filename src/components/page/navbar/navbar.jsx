cc0620cf82a45ae934e44c63a5b3ac17c5ba729d
minhtrungs013 <trungs0132@gmail.com>
2024-01-24 17:05:56 +0700

    ass

--- src/components/page/navbar/navbar.jsx
+++ src/components/page/navbar/navbar.jsx
@@ -4,22 +4,22 @@ const menuitems = [
     {
         id: 0,
         title: "Dịch vụ",
-        path: "/dich-vu",
+        path: "GabiSpa/dich-vu",
     },
     {
         id: 1,
         title: "Công Việc",
-        path: "/cong-viec",
+        path: "GabiSpa/cong-viec",
     },
     {
         id: 2,
         title: "Tin Tức",
-        path: "/tin-tuc",
+        path: "GabiSpa/tin-tuc",
     },
     {
         id: 3,
         title: "Liên Hệ",
-        path: "/lien-he",
+        path: "GabiSpa/lien-he",
     },
 ];
 
@@ -27,7 +27,7 @@ export default function Navbar() {
     return (
         <div className='flex flex-col lg:flex-row justify-between items-center my-5'>
             <div className="flex w-full lg:w-auto items-center justify-between">
-                <Link to={'/'} className="text-lg">
+                <Link to={'GabiSpa/'} className="text-lg">
                     <img className='' src="https://res.cloudinary.com/dax8xvyhi/image/upload/c_fill,h_90,w_250/v1705774032/mdggvkgqlr2osrxdt1hh.png" alt="" />
                 </Link>
             </div>
@@ -46,8 +46,8 @@ export default function Navbar() {
             </nav>
             <div>
                 <div className="hidden lg:flex items-center gap-4">
-                    <Link to={'/'} >Đăng nhập</Link>
-                    <Link to={'/'} size="md" className='rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-4 py-2 bg-[#214581] text-white hover:bg-[#2e4d81]  border-2 border-transparent'>Đăng ký</Link>
+                    <Link to={'GabiSpa/'} >Đăng nhập</Link>
+                    <Link to={'GabiSpa/'} size="md" className='rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-4 py-2 bg-[#214581] text-white hover:bg-[#2e4d81]  border-2 border-transparent'>Đăng ký</Link>
                 </div>
             </div>
         </div >
