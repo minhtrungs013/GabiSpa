91ca7125d7d05be2bb909f6d64e2d088b51e32e4
minhtrungs013 <trungs0132@gmail.com>
2024-01-29 03:41:05 +0700

    123

--- src/components/page/navbar/navbar.jsx
+++ src/components/page/navbar/navbar.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Login from '../../login/login';
+import Register from '../../register/register';
 const menuitems = [
     {
         id: 0,
@@ -28,54 +29,73 @@ const menuitems = [
 
 export default function Navbar() {
     const [open, setOpen] = useState(false)
-    const [isModalVisible, setModalVisibility] = useState(false);
+    const [isModalLogin, setModalLogin] = useState(false);
+    const [isModalRegister, setModalRegister] = useState(false);
 
-    const openModal = () => {
-        setModalVisibility(true);
+    const openModalLogin = () => {
+        setModalLogin(true);
     };
 
-    const closeModal = () => {
-        setModalVisibility(false);
+    const closeModalLogin = () => {
+        setModalLogin(false);
     };
+
+    const openModalRegister = () => {
+        setModalRegister(true);
+    };
+
+    const closeModalRegister = () => {
+        setModalRegister(false);
+    };
+
+    const switchLogin = () => {
+        setModalRegister(false);
+        setModalLogin(true);
+    }
+    const switchRegister = () => {
+        setModalLogin(false);
+        setModalRegister(true);
+    }
 
     return (
         <>
-        <div className='flex flex-col lg:flex-row justify-between items-center my-5'>
-            <div className="flex w-full lg:w-auto items-center justify-between">
-                <Link to={'/GabiSpa'} className="text-lg">
-                    <img className='' src="https://res.cloudinary.com/dax8xvyhi/image/upload/c_fill,h_90,w_250/v1705774032/mdggvkgqlr2osrxdt1hh.png" alt="" />
-                </Link>
-                <div class="block lg:hidden">
-                    <button class="text-gray-800" onClick={() => setOpen(!open)}>
-                        <FontAwesomeIcon icon={faBars} className='pr-3 text-4xl text-[#214581]' />
-                    </button>
+            <div className='flex flex-col lg:flex-row justify-between items-center my-5'>
+                <div className="flex w-full lg:w-auto items-center justify-between">
+                    <Link to={'/GabiSpa'} className="text-lg">
+                        <img className='' src="https://res.cloudinary.com/dax8xvyhi/image/upload/c_fill,h_90,w_250/v1705774032/mdggvkgqlr2osrxdt1hh.png" alt="" />
+                    </Link>
+                    <div class="block lg:hidden">
+                        <button class="text-gray-800" onClick={() => setOpen(!open)}>
+                            <FontAwesomeIcon icon={faBars} className='pr-3 text-4xl text-[#214581]' />
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <nav className="hidden w-full lg:w-auto mt-2 lg:!flex lg:mt-0" style={{ display: open ? 'block' : 'none' }}>
-                <ul className="flex flex-col lg:flex-row lg:gap-3" >
-                    {menuitems.map((item) => (
-                        <li key={item.id}>
-                            <Link
-                                to={`${item.path}`}
-                                className="flex lg:px-2 xl:px-3 py-2 text-gray-600 hover:text-[#214581] font-medium text-xl ">
-                                {item.title}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-                <div class="lg:!hidden flex items-center justify-center mt-3 gap-4 " style={{ display: open ? 'flex' : 'none' }}>
-                    <Link to={'/GabiSpa'} className='w-full rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-4 py-2 ] text-black border-[1px] border-[#2e4d81]' >Đăng nhập</Link>
-                    <Link to={'/GabiSpa'} size="md" className='w-full rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-4 py-2 bg-[#214581] text-white hover:bg-[#2e4d81]  border-2 border-transparent'>Đăng ký</Link>
+                <nav className="hidden w-full lg:w-auto mt-2 lg:!flex lg:mt-0" style={{ display: open ? 'block' : 'none' }}>
+                    <ul className="flex flex-col lg:flex-row lg:gap-3" >
+                        {menuitems.map((item) => (
+                            <li key={item.id}>
+                                <Link
+                                    to={`${item.path}`}
+                                    className="flex lg:px-2 xl:px-3 py-2 text-gray-600 hover:text-[#214581] font-medium text-xl ">
+                                    {item.title}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <div class="lg:!hidden flex items-center justify-center mt-3 gap-4 " style={{ display: open ? 'flex' : 'none' }}>
+                        <Link to={'/GabiSpa'} className='w-full rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-4 py-2 ] text-black border-[1px] border-[#2e4d81]' >Đăng nhập</Link>
+                        <Link to={'/GabiSpa'} size="md" className='w-full rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-4 py-2 bg-[#214581] text-white hover:bg-[#2e4d81]  border-2 border-transparent'>Đăng ký</Link>
+                    </div>
+                </nav>
+                <div>
+                    <div className="hidden lg:flex items-center gap-4" >
+                        <Link to={'/GabiSpa'} onClick={openModalLogin} className=' rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-4 py-2 ] text-black border-[1px] border-[#2e4d81]' >Đăng nhập</Link>
+                        <Link to={'/GabiSpa'} onClick={openModalRegister} size="md" className='rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-4 py-2 bg-[#214581] text-white hover:bg-[#2e4d81]  border-2 border-transparent'>Đăng ký</Link>
+                    </div>
                 </div>
-            </nav>
-            <div>
-                <div className="hidden lg:flex items-center gap-4" >
-                    <Link to={'/GabiSpa'} onClick={openModal} className=' rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-4 py-2 ] text-black border-[1px] border-[#2e4d81]' >Đăng nhập</Link>
-                    <Link to={'/GabiSpa'} size="md" className='rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-4 py-2 bg-[#214581] text-white hover:bg-[#2e4d81]  border-2 border-transparent'>Đăng ký</Link>
-                </div>
-            </div>
-        </div >
-        <Login closeModal={closeModal} showModal={isModalVisible}/>
+            </div >
+            <Login closeModal={closeModalLogin} showModal={isModalLogin} switchRegister={switchRegister} />
+            <Register closeModal={closeModalRegister} showModal={isModalRegister} switchLogin={switchLogin} />
         </>
     )
 }
--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -3,7 +3,7 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useCallback, useEffect } from 'react';
 
-export default function Login({ showModal, closeModal, switchRegister }) {
+export default function Register({ showModal, closeModal , switchLogin}) {
     const handleKeyDown = useCallback((e) => {
         if (e.key === 'Escape') {
             closeModal();
@@ -33,28 +33,35 @@ export default function Login({ showModal, closeModal, switchRegister }) {
                             <div className="max-w-md mx-auto">
                                 <div className='flex text-center items-center '>
                                     <img className='h-[80px]' src={"https://res.cloudinary.com/dax8xvyhi/image/upload/v1705772414/b7hrtq1xljrctwe089kv.png"} alt="" />
-                                    <h1 className="text-2xl font-semibold">Đăng Nhập</h1>
+                                    <h1 className="text-2xl font-semibold">Đăng ký</h1>
                                 </div>
                                 <div className="divide-y divide-gray-200">
                                     <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                                         <div className="relative">
-                                            <input autocomplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 text-sm" placeholder="Tên Đăng Nhập" />
+                                            <input autocomplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 text-sm" placeholder="Tài Khoản" />
                                             <label for="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Tên Đăng Nhập </label>
+                                        </div>
+                                        <div className="relative">
+                                            <input autocomplete="off" id="password" name="text" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 text-sm" placeholder="Mật Khẩu" />
+                                            <label for="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Địa Chỉ</label>
                                         </div>
                                         <div className="relative">
                                             <input autocomplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 text-sm" placeholder="Mật Khẩu" />
                                             <label for="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Mật Khẩu</label>
                                         </div>
                                         <div className="relative">
-                                            <button className="bg-[#214581] text-white w-32 rounded-md px-2 py-1">Đăng Nhập</button>
+                                            <input autocomplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 text-sm" placeholder="Mật Khẩu" />
+                                            <label for="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Nhập Lại Mật Khẩu</label>
+                                        </div>
+                                        <div className="relative">
+                                            <button className="bg-[#214581] text-white w-32 rounded-md px-2 py-1 text-base">Xác Nhận</button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
                             <div className="w-full flex justify-center">
-                                <span>bạn chưa có tài khoản? </span>
-                                <span className='pl-2 text-blue-700 underline cursor-pointer' onClick={switchRegister}>Đăng Ký </span>
+                                <span>bạn đã có tài khoản? </span>
+                                <span className='pl-2 text-blue-700 underline cursor-pointer' onClick={switchLogin}>Đăng Nhập </span>
                             </div>
                         </div>
                     </div>
